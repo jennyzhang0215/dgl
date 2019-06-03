e0f79d443e0610a77bf6b1f122494ed9eaaed0c1
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-03 15:29:54 +0800

    update

--- examples/mxnet/gc-mc/testing.py
+++ examples/mxnet/gc-mc/testing.py
@@ -15,11 +15,14 @@ import dgl
 3 |  |2 |  |  |5 |
 """
 def gen_bipartite():
+    n_user = 4
+    n_item = 5
     user_item_R = np.array([[1,2,0,4,0], [0,0,3,0,5], [1,0,0,4,0], [0,2,0,0,5]])
     user_item_coo = sp.coo_matrix(user_item_R)
     g = dgl.DGLBipartiteGraph(metagraph = nx.MultiGraph([('user', 'item', 'rating')]),
-                              num_typed_nodes = {'user': 4, 'item': 5},
+                              number_of_nodes_by_type = {'user': n_user, 'item': n_item},
                               edge_connections_by_type = {('user', 'item', 'rating'): user_item_coo},
+                              node_frame = {"user": np.eye(n_user), "item": np.eye(n_item)},
                               readonly = True)
     print("#users: {}".format(g['user'].number_of_nodes()))
     print("#items: {}".format(g['item'].number_of_nodes()))
