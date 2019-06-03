84cda94e3aa99518c17b0a19bfe2f260abc4c169
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-03 18:21:51 +0800

    update

--- examples/mxnet/gc-mc/testing.py
+++ examples/mxnet/gc-mc/testing.py
@@ -21,7 +21,6 @@ def gen_bipartite():
     user_item_pair = np.array([[0, 0, 0, 1, 1, 2, 2, 3, 3],
                                [0, 1, 3, 2, 4, 0, 3, 1, 4]])
     user_item_ratings = np.array([1,2,4,3,5,1,4,2,5])
-    user_item_coo = sp.coo_matrix(user_item_R)
     g = dgl.DGLBipartiteGraph(metagraph = nx.MultiGraph([('user', 'item', 'rating')]),
                               number_of_nodes_by_type = {'user': n_user, 'item': n_item},
                               edge_connections_by_type = {('user', 'item', 'rating'): (user_item_pair[0, :],
@@ -29,9 +28,10 @@ def gen_bipartite():
                               node_frame = {"user": np.eye(n_user), "item": np.eye(n_item)},
                               readonly = True)
     g.edata["rating"] =  user_item_ratings
-    print("#users: {}".format(g['user'].number_of_nodes()))
-    print("#items: {}".format(g['item'].number_of_nodes()))
+    print("#users: {}".format(g['user'].number_of_nodes()), g['user'].nodes)
+    print("#items: {}".format(g['item'].number_of_nodes()), g['item'].nodes)
     print("#ratings: {}".format(g.number_of_edges()))
+    print("g.edges", g.edges)
     print("g.edata", g.edata)
 
     return g
