56507b3764ff0e8e40e86a23e7cc1d685e8452b3
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-03 21:53:08 +0800

    update

--- examples/mxnet/gc-mc/testing.py
+++ examples/mxnet/gc-mc/testing.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.sparse as sp
 import networkx as nx
 import dgl
+import sys
 
 
 """
@@ -14,6 +15,50 @@ import dgl
 2 |1 |  |  |4 |  |
 3 |  |2 |  |  |5 |
 """
+
+def _globally_normalize_bipartite_adjacency(adjacencies, symmetric=True):
+    """ Globally Normalizes set of bipartite adjacency matrices """
+
+    # degree_u and degree_v are row and column sums of adj+I
+
+    adj_tot = np.sum(adj for adj in adjacencies) ## it is just the original training adj
+    degree_u = np.asarray(adj_tot.sum(1)).flatten()
+    degree_v = np.asarray(adj_tot.sum(0)).flatten()
+
+    # set zeros to inf to avoid dividing by zero
+    degree_u[degree_u == 0.] = np.inf
+    degree_v[degree_v == 0.] = np.inf
+
+    degree_u_inv_sqrt = 1. / np.sqrt(degree_u)
+    degree_v_inv_sqrt = 1. / np.sqrt(degree_v)
+    degree_u_inv_sqrt_mat = sp.diags([degree_u_inv_sqrt], [0])
+    degree_v_inv_sqrt_mat = sp.diags([degree_v_inv_sqrt], [0])
+
+    degree_u_inv = degree_u_inv_sqrt_mat.dot(degree_u_inv_sqrt_mat)
+
+    if symmetric:
+        adj_norm = [degree_u_inv_sqrt_mat.dot(adj).dot(degree_v_inv_sqrt_mat) for adj in adjacencies]
+
+    else:
+        adj_norm = [degree_u_inv.dot(adj) for adj in adjacencies]
+
+    return adj_norm
+
+def compute_support(adj_train, num_link, symmetric):
+    support = []
+    adj_train_int = sp.csr_matrix(adj_train, dtype=np.int32)
+    for i in range(num_link):
+        # build individual binary rating matrices (supports) for each rating
+        support_unnormalized = sp.csr_matrix(adj_train_int == i + 1, dtype=np.float32)
+        support.append(support_unnormalized)
+
+    support = _globally_normalize_bipartite_adjacency(support, symmetric=symmetric)
+
+    num_support = len(support)
+    print("num_support:", num_support, support)
+    #support = sp.hstack(support, format='csr')
+    return support
+
 def gen_bipartite():
     n_user = 4
     n_item = 5
@@ -25,10 +70,9 @@ def gen_bipartite():
                               number_of_nodes_by_type = {'user': n_user, 'item': n_item},
                               edge_connections_by_type = {('user', 'item', 'rating'): (user_item_pair[0, :],
                                                                                        user_item_pair[1, :])},
-                              node_frame = {"user": np.eye(n_user), "item": np.eye(n_item)},
-                              edge_frame = {"rating2": user_item_ratings},
                               readonly = True)
-    g.edata["rating"] =  user_item_ratings
+    g.edata["rating"] = user_item_ratings
+
     print("#users: {}".format(g['user'].number_of_nodes()))
     print("#items: {}".format(g['item'].number_of_nodes()))
     print("#ratings: {}".format(g.number_of_edges()))
@@ -38,6 +82,8 @@ def gen_bipartite():
     # print("g.edges('uv', 'srcdst')", g.edges('uv', 'srcdst'))
     print("g.edata", g.edata)
 
+    compute_support(user_item_R, 5, True)
+    
     return g
 
 
