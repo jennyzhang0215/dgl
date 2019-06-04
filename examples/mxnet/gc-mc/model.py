62ee2760f1437c624cb42c2fb6476aacbfe64554
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-04 09:18:50 +0800

    update

--- examples/mxnet/gc-mc/model.py
+++ examples/mxnet/gc-mc/model.py
@@ -1,13 +1,48 @@
 from mxnet as gluon
 import mxnet.ndarray as F
-import dgl.function as fn
-from gluon import nn
 import numpy as np
+import warnings
+from mxnet.gluon import nn, HybridBlock, Block
 
 from utils import get_activation
 
-class MultiLinkGCNAggregator(gluon.Block):
-    def __init__(self, agg_units, num_links,
+
+class LayerDictionary(Block):
+    def __init__(self, **kwargs):
+        """
+
+        Parameters
+        ----------
+        input_dims : dict
+        output_dims : dict
+        """
+        super(LayerDictionary, self).__init__(**kwargs)
+        self._key2idx = dict()
+        with self.name_scope():
+            self._layers = nn.HybridSequential()
+        self._nlayers = 0
+
+    def __len__(self):
+        return len(self._layers)
+
+    def __setitem__(self, key, layer):
+        if key in self._key2idx:
+            warnings.warn('Duplicate Key. Need to test the code!')
+            self._layers[self._key2idx[key]] = layer
+        else:
+            self._layers.add(layer)
+            self._key2idx[key] = self._nlayers
+            self._nlayers += 1
+
+    def __getitem__(self, key):
+        return self._layers[self._key2idx[key]]
+
+    def __contains__(self, key):
+        return key in self._key2idx
+
+
+class MultiLinkGCNAggregator(Block):
+    def __init__(self, units, num_links,
                  dropout_rate=0.0, accum='stack', act=None, **kwargs):
         super(MultiLinkGCNAggregator, self).__init__(**kwargs)
 
@@ -19,12 +54,12 @@ class MultiLinkGCNAggregator(gluon.Block):
             for i in range(num_links):
                 self.__setattr__('weight{}'.format(i),
                                  self.params.get('weight{}'.format(i),
-                                                 shape=(agg_units, 0),
+                                                 shape=(units, 0),
                                                  dtype=np.float32,
                                                  allow_deferred_init=True))
                 self.__setattr__('bias{}'.format(i),
                                  self.params.get('bias{}'.format(i),
-                                                 shape=(agg_units,),
+                                                 shape=(units,),
                                                  dtype=np.float32,
                                                  init='zeros',
                                                  allow_deferred_init=True))
@@ -63,6 +98,48 @@ class MultiLinkGCNAggregator(gluon.Block):
         return h
 
 
-class GCMCLayer():
-    def __init__(self):
+class GCMCLayer(Block):
+    def __init__(self, agg_units, out_units, num_links, src_key, dst_key,
+                 dropout_rate=0.0, agg_accum='stack', agg_act=None,
+                 agg_ordinal_sharing=False, share_agg_weights=False,
+                 share_out_fc_weights = False, out_act = None,
+                 **kwargs):
+        super(GCMCLayer, self).__init__(**kwargs)
+        self._out_act = get_activation(out_act)
+        with self.name_scope():
+            self.dropout = nn.Dropout(dropout_rate) ### dropout before feeding the out layer
+            self._aggregators = LayerDictionary(prefix='agg_')
+            with self._aggregators.name_scope():
+                self._aggregators[(src_key, dst_key)] = MultiLinkGCNAggregator(units = agg_units,
+                                                                               num_links=num_links,
+                                                                               dropout_rate=dropout_rate,
+                                                                               accum=agg_accum,
+                                                                               act=agg_act,
+                                                                               prefix='{}_{}_'.format(src_key, dst_key))
+                self._aggregators[(dst_key, src_key)] = MultiLinkGCNAggregator(units=agg_units,
+                                                                               num_links=num_links,
+                                                                               dropout_rate=dropout_rate,
+                                                                               accum=agg_accum,
+                                                                               act=agg_act,
+                                                                               prefix='{}_{}_'.format(dst_key, src_key))
+            self._out_fcs = LayerDictionary(prefix='out_fc_')
+            with self._out_fcs.name_scope():
+                self._out_fcs[src_key] = nn.Dense(out_units, flatten=False,
+                                                  prefix='{}_'.format(src_key))
+                self._out_fcs[dst_key] = nn.Dense(out_units, flatten=False,
+                                                  prefix='{}_'.format(dst_key))
+
+            self._out_act = get_activation(out_act)
+
+    def forward(self, src_g, dst_g, src_key, dst_key):
+        dst_h = self._aggregators[(src_key, dst_key)](src_g, dst_key)
+        src_h = self._aggregators[(dst_key, src_key)](dst_g, src_key)
+        out_dst = self._out_act(self._out_fcs[dst_key](dst_h))
+        out_src = self._out_act(self._out_fcs[dst_key](src_h))
+        src_g[src_key].ndata['h'] = out_src
+        src_g[dst_key].ndata['h'] = out_dst
+        dst_g[src_key].ndata['h'] = out_dst
+        dst_g[dst_key].ndata['h'] = out_src
+        return out_src, out_dst
+
 
