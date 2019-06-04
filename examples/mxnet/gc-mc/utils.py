161dc854c279a7a57ef6408614164597dd7eca79
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-04 10:43:29 +0800

    update

--- examples/mxnet/gc-mc/utils.py
+++ examples/mxnet/gc-mc/utils.py
@@ -1,6 +1,6 @@
 import ast
 import os
-import io
+import csv
 import inspect
 import logging
 import re
@@ -11,7 +11,23 @@ import numpy as np
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.linear_model import SGDClassifier
+from collections import OrderedDict
 
+class MetricLogger(object):
+    def __init__(self, attr_names, parse_formats, save_path):
+        self._attr_format_dict = OrderedDict(zip(attr_names, parse_formats))
+        self._file = open(save_path, 'w')
+        self._csv = csv.writer(self._file)
+        self._csv.writerow(attr_names)
+        self._file.flush()
+
+    def log(self, **kwargs):
+        self._csv.writerow([parse_format % kwargs[attr_name]
+                            for attr_name, parse_format in self._attr_format_dict.items()])
+        self._file.flush()
+
+    def close(self):
+        self._file.close()
 
 def copy_params_to_nd(params, ctx=None):
     return {k: v.data(ctx).copy() for k, v in params.items()}
