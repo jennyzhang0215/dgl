6f72a15c35e4039c35c9cbebc9bc7101a76d8954
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-24 16:52:40 +0800

    update

--- examples/mxnet/gc-mc/testing.py
+++ examples/mxnet/gc-mc/testing.py
@@ -113,12 +113,12 @@ def gen_bipartite():
     #print("g['user']", g['user'].ndata)
     #print("g2['user']", g2['user'].ndata)
     #print("g2['item']", g2['item'].ndata)
-    print(g2["user"].ndata.pop('res'))
-
+    print(g2["item"].ndata.pop('res'))
 
     g1.send_and_recv(g1.edges(),
                      msg_func, fn.sum("m", "accum"), apply_node_func)
     print(g1["item"].ndata.pop('res'))
+    print(g1["user"].ndata.pop('res'))
 
 
 
