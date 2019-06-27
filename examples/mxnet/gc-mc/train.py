a79b960e42ca4e3320e68088e72546403cfbd77a
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-06-27 15:07:13 +0800

    update

--- examples/mxnet/gc-mc/train.py
+++ examples/mxnet/gc-mc/train.py
@@ -217,7 +217,7 @@ def config():
     parser.add_argument('--save_id', type=int, help='The saving log id')
     parser.add_argument('--silent', action='store_true')
 
-    parser.add_argument('--data_name', default='ml-1m', type=str,
+    parser.add_argument('--data_name', default='ml-100k', type=str,
                         help='The dataset name: ml-100k, ml-1m, ml-10m')
     parser.add_argument('--data_test_ratio', type=float, default=0.1) ## for ml-100k the test ration is 0.2
     parser.add_argument('--data_valid_ratio', type=float, default=0.1)
