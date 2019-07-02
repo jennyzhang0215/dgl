b39860b05c70e91e52fccc099b93834b688ca9d7
jennyzhang0215 <jennyzhang0215@gmail.com>
2019-07-02 09:20:12 +0800

    update

--- examples/mxnet/gc-mc/train.py
+++ examples/mxnet/gc-mc/train.py
@@ -241,7 +241,7 @@ def config():
     parser.add_argument('--gen_r_num_basis_func', type=int, default=2)
 
     # parser.add_argument('--train_rating_batch_size', type=int, default=10000)
-    parser.add_argument('--train_max_iter', type=int, default=500)
+    parser.add_argument('--train_max_iter', type=int, default=2000)
     parser.add_argument('--train_log_interval', type=int, default=1)
     parser.add_argument('--train_valid_interval', type=int, default=1)
     parser.add_argument('--train_optimizer', type=str, default="adam")
